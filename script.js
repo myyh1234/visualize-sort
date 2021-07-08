c8fca32eb092f1dd0b623655cd6c25ff53d2febd
myyh1234 <ccw040607@gmail.com>
2021-07-08 22:30:24 +0900

    add bubbleSort

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-con = document.getElementById("container")
-btn = document.getElementById("btn")
-algo = document.getElementById("algorithm")
-li = []
+const con = document.getElementById("container")
+const btn = document.getElementById("btn")
+const algo = document.getElementById("algorithm")
+
+let a, b
+let interval
+
+const build = (arr) => {
+    // console.log('build in')
+    while (con.children.length > 0)
+        con.removeChild(con.firstChild)
+    
+    for (let i = 0; i < 100; i++) {
+        const now = document.createElement("div")
+        now.style.width = "10px"
+        now.style.backgroundColor = "hsl(" + 3 * arr[i] + ", 100%, 50%)"
+        now.id = arr[i] + ''
+        now.className = "element"
+        con.appendChild(now)
+    }
+    // console.log('build out')
+}
+
+const sleep = (t) => {
+    console.log('in')
+    const now = new Date().getTime()
+    while (new Date().getTime() < now + t){
+        
+    }
+    console.log('out')
+}
 
 const tmp = []
 for (let i = 0; i < 100; i++){
@@ -10,45 +37,39 @@ for (let i = 0; i < 100; i++){
 
 const arr = _.shuffle(tmp)
 
-for (let i = 0; i < 100; i++){
-    const now = document.createElement("div")
-    now.style.width = "10px"
-    now.style.backgroundColor = "hsl(" + arr[i] + ", 100%, 50%)"
-    now.style.order = i + ''
-    now.id = arr[i] + ''
-    now.className = "element"
-    con.appendChild(now)
-}
+build(arr)
 
-swap_ele = (i, j, li) => {
-    let tmp = li[i].style.order
-    li[i].style.order = li[j].style.order
-    li[j].style.order = tmp
 
-    tmp = li[i]
-    li[i] = li[j]
-    li[j] = li[i]
-}
-
-bubbleSort = () => {
-    li = con.children
-    li.sort()
-    console.log(li)
-    for (let i = 0; i < 100; i++){
-        for (let j = 0; j < 99; j++){
-            if (parseInt(li[j].id) > parseInt(li[j + 1].id)){
-                swap_ele(j, j + 1, li)
-            }
+bubbleSort = (arr) => {
+    b++
+    if (b == 99 - a){
+        a++
+        b = 0
+        if (a == 99){
+            clearInterval(interval)
+            alert('finish!')
         }
     }
-    console.log('fin')
+    console.log(a, b)
+
+    if (arr[b] > arr[b + 1]){
+        let tmp = con.children[b].style.backgroundColor
+        con.children[b].style.backgroundColor = con.children[b + 1].style.backgroundColor
+        con.children[b + 1].style.backgroundColor = tmp
+        tmp = arr[b]
+        arr[b] = arr[b + 1]
+        arr[b + 1] = tmp
+    }
 }
 
+
+
 btn.onclick = () => {
-    li = con.children
-    
+    clearInterval(interval)
     switch(algo.value){
         case "bubble":
-            bubbleSort()
+            interval = setInterval(() => {
+                bubbleSort(arr)
+            }, 5);
     }
 }
